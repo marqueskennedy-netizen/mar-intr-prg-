7bb31a8fecef122fe7fe66e25b5b5ee9793e7ef5
marqueskennedy-netizen <marques.kennedy@escola.pr.gov.br>
2026-05-26 09:09:46 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,13 @@
+function lutar(){
+    const classe= document.getElementById('classe').Value;
+    const arma=document.getElementById('arma').value.trim();
+    const log= document.getElementById('log');
+
+    if(!arma){
+        log.innerHTML= "Você esqueceu de equipar uma arma"
+
+    }
+    let dado = matchMedia.floor(matchMedia.radom() * 20) + 1
+
+    0,4563 *20 +1
+}
